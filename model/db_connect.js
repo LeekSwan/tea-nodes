29a256b5b315e99c1632727ab9aa6d806f10e9e1
Leek Swan <lixuanron@gmail.com>
2022-12-23 15:07:28 -0500

    refactor db_connect

--- model/db_connect.js
+++ model/db_connect.js
@@ -1,16 +1,35 @@
 const { Pool } = require('pg');
 
-// Create a connection pool using the connection information provided on bit.io.
-function connect() {
-    console.log('here at connect')
-    return new Pool({
-        user: 'rabidrabbit',
-        host: 'db.bit.io',
-        database: 'rabidrabbit/tea-db', // public database 
-        password: 'v2_3x7yt_YRZX48KNswH4Uq8jLADWKee', // key from bit.io database page connect menu
-        port: 5432,
-        ssl: true,
-    });
+let client = null
+// Returns a Promise for a connected PG Client instance
+// If no client exists, it creates one
+// Otherwise, returns existing client (cached)
+// NOTE: Be sure to await or .then(), else you will be very confused
+async function getClient () {
+  if (client) {
+    return client
+  }
+  try {
+    client = new Pool({
+            user: 'rabidrabbit',
+            host: 'db.bit.io',
+            database: 'rabidrabbit/tea-db', // public database 
+            password: 'v2_3x7yt_YRZX48KNswH4Uq8jLADWKee', // key from bit.io database page connect menu
+            port: 5432,
+            ssl: true,
+        });
+    await client.connect()
+    return client
+  } catch (err) {
+    console.log('Error when connecting to PG:')
+    console.log(err.message)
+    return null
+  }
 }
 
-module.exports = { connect }
+async function query (text) {
+    const client = await getClient()
+    return client.query(text)
+  }
+
+module.exports = { query }
--- model/db_queries.js
+++ model/db_queries.js
@@ -1,21 +1,16 @@
-const  {connect} = require('./db_connect')
+const  db = require('./db_connect')
 
 module.exports = {
-
     get_all_tea_nodes: async function () {
         const getAllTeas = {text: 'SELECT * FROM tea_nodes'}
-        const result = await connect().query(getAllTeas)
+        const result = await db.query(getAllTeas)
         return result.rows
     },
 
     //Gets all edges from database
     get_all_edges: async function () {
         const getAllEdges = {text: 'SELECT * FROM edges'}
-        const result = await connect().query(getAllEdges)
+        const result = await db.query(getAllEdges)
         return result.rows
     }
 }
-
-
-
-
